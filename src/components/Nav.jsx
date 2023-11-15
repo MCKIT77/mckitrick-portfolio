4958d70eb3858148a87733ccf6cc36808a985083
Jacob McKitrick <jakemckit@gmail.com>
2023-11-15 18:37:10 -0500

    fixed nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
-import Navbar from './UI/Navbar';
-import '../styles/Nav.css'
+import Navbar from './UI/Navbar.jsx';
+import '../styles/Nav.css';
 
 export default function Nav() {
 
