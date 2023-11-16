bb8c82e8c54413c5e3fc8b24d49fcc823f74aa4b
Jacob McKitrick <jakemckit@gmail.com>
2023-11-15 19:53:09 -0500

    updated portfolio section and resume section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,33 +1,101 @@
+// import spacebook from '../assets/Spacebook_screenshot.png'
+// import mealmagic from '../assets/MealMagic.png'
+// import decoys from '../assets/Trippy_Pups_30.png'
+// import instaclip from '../assets/insta-clip.jpg'
+// import '../styles/Portfolio.css'
+
+// export default function Portfolio() {
+
+//     return (
+//         <section id="portfolio">
+//             <h3>My Work</h3>
+//             <div class="projects">
+//                 <section class="card" id="firstCard">
+//                     <h4>Spacebook</h4>
+//                     <a href="https://spacebook-socialmedia-app-9c15fa4095df.herokuapp.com/" target="_blank"><img src={spacebook} alt="spacebook" /></a>
+//                 </section>
+//                 <section class="card">
+//                     <h4>Meal Magic</h4>
+//                     <a href="https://mckit77.github.io/meal-magic/" target="_blank"><img src={mealmagic} alt="meal-magic" /></a>
+//                 </section>
+//                 <section class="card">
+//                     <h4>InstaClip</h4>
+//                     <a href="https://instaclip-5c26d78800f1.herokuapp.com/login" target="_blank"><img src={instaclip} alt='instaclip' /></a>
+//                 </section>
+//                 <section class="card">
+//                     <h4>Project 4</h4>
+//                     <a href="https://www.youtube.com/watch?v=iik25wqIuFo" target="_blank"><img src={decoys} /></a>
+//                 </section>
+//                 <section class="card">
+//                     <h4>Project 5</h4>
+//                     <a href="https://www.youtube.com/watch?v=iik25wqIuFo" target="_blank"><img src={decoys} /></a>
+//                 </section>
+//             </div>
+//         </section>
+//     );
+// }
+
+//new try
 import spacebook from '../assets/Spacebook_screenshot.png'
 import mealmagic from '../assets/MealMagic.png'
 import decoys from '../assets/Trippy_Pups_30.png'
+import instaclip from '../assets/insta-clip.jpg'
 import '../styles/Portfolio.css'
+import React, { useState, useEffect } from 'react';
+import '../styles/Portfolio.css';
 
 export default function Portfolio() {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        // Fetch GitHub repositories using the GitHub API
+        const fetchRepos = async () => {
+            try {
+                const response = await fetch('https://api.github.com/users/MCKIT77/repos');
+                const data = await response.json();
+                setRepos(data);
+            } catch (error) {
+                console.error('Error fetching GitHub repositories:', error);
+            }
+        };
+
+        fetchRepos();
+    }, []);
 
     return (
         <section id="portfolio">
-            <h3>My Work</h3>
-            <div class="projects">
-                <section class="card" id="firstCard">
+            <h3>My Projects</h3>
+            <div className="projects">
+                {/* Manually included projects */}
+                <section className="card" id="firstCard">
                     <h4>Spacebook</h4>
-                    <a href="https://spacebook-socialmedia-app-9c15fa4095df.herokuapp.com/" target="_blank"><img src={spacebook} alt="spacebook" /></a>
+                    <a href="https://spacebook-socialmedia-app-9c15fa4095df.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <img src={spacebook} alt="spacebook" />
+                    </a>
                 </section>
-                <section class="card">
+                <section className="card">
                     <h4>Meal Magic</h4>
-                    <a href="https://mckit77.github.io/meal-magic/" target="_blank"><img src={mealmagic} alt="meal-magic" /></a>
+                    <a href="https://mckit77.github.io/meal-magic/" target="_blank" rel="noopener noreferrer">
+                        <img src={mealmagic} alt="meal-magic" />
+                    </a>
                 </section>
-                <section class="card">
-                    <h4>Project 3</h4>
-                    <a href="https://www.youtube.com/watch?v=iik25wqIuFo" target="_blank"><img src={decoys} /></a>
+                <section className="card">
+                    <h4>InstaClip</h4>
+                    <a href="https://instaclip-5c26d78800f1.herokuapp.com/login" target="_blank" rel="noopener noreferrer">
+                        <img src={instaclip} alt='instaclip' />
+                    </a>
                 </section>
-                <section class="card">
-                    <h4>Project 4</h4>
-                    <a href="https://www.youtube.com/watch?v=iik25wqIuFo" target="_blank"><img src={decoys} /></a>
-                </section>
-                <section class="card">
-                    <h4>Project 5</h4>
-                    <a href="https://www.youtube.com/watch?v=iik25wqIuFo" target="_blank"><img src={decoys} /></a>
+                {/* Dynamically fetched GitHub repositories */}
+                <section id="dynamicRepos" className="card">
+                    <h3>All Repos</h3>
+                    {repos.map(repo => (
+                        <section className="card" key={repo.id}>
+                            <h4>{repo.name}</h4>
+                            <a href={repo.html_url} target="_blank" rel="noopener noreferrer">
+                                <img src={`https://raw.githubusercontent.com/MCKIT77/${repo.name}/main/path/to/your/image/file.jpg`} alt={repo.name} />
+                            </a>
+                        </section>
+                    ))}
                 </section>
             </div>
         </section>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,16 +1,27 @@
 import Resume1 from '../assets/Resume_p1.jpg'
 import Resume2 from '../assets/Resume_p2.jpg'
 import Resume3 from '../assets/Resume_p3.jpg'
+import osucert from '../assets/osu-cert.jpg'
+import resumefile from '../assets/files/Jacob-McKitrick-Resume.pdf'
 import '../styles/Resume.css'
 
 export default function Contact() {
 
     return (
         <resume id="resume">
+            <h3>Full Stack Certificate</h3>
+            <figure><img src={osucert} alt="osu certificate" /></figure>
             <h3>Resume</h3>
+
             <figure><img src={Resume1} alt="resume" /></figure>
             <figure><img src={Resume2} alt="resume" /></figure>
             <figure><img src={Resume3} alt="resume" /></figure>
+            <div className="resume-download">
+                <h4>Download Resume</h4>
+                <a className='file' href={resumefile} download="Jacob-McKitrick-Resume.pdf">
+                    <button>Download PDF</button>
+                </a>
+            </div>
         </resume>
     );
 }
